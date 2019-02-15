93d44cbbbf06a8d4be510b658dd8b001b2f8222f
Ivan Nikiforov <nikiforr@gmail.com>
2019-02-15 20:34:34 +0300

    Parse and process command-line arguments

--- client.js
+++ client.js
@@ -5,6 +5,21 @@ var booksProto = grpc.load('books.proto');
 var client = new booksProto.books.BookService('127.0.0.1:50051',
 	grpc.credentials.createInsecure());
 
+var processName = process.argv.shift();
+var scriptName = process.argv.shift();
+var command = process.argv.shift();
+
+if (command == 'list')
+	listBooks();
+else if (command == 'insert')
+	insertBook(process.argv[0], process.argv[1], process.argv[2]);
+else if (command == 'get')
+	getBook(process.argv[0]);
+else if (command == 'delete')
+	deleteBook(process.argv[0]);
+else if (command == 'watch')
+	watchBooks();
+
 function printResponse(error, response) {
 	if (error)
 		console.log('Error: ', error);
@@ -34,7 +49,7 @@ function getBook(id) {
 
 function deleteBook(id) {
 	var bookId = { id: parseInt(id) }
-	client.delete(bookId, function(error, epmty) {
+	client.delete(bookId, function(error, empty) {
 		printResponse(error, empty);
 	});
 }
