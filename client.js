5a767d745b5d68854a802963bd1ea95412775321
Ivan Nikiforov <nikiforr@gmail.com>
2019-02-15 20:17:14 +0300

    Add CLI helpers for CRUDL

--- client.js
+++ client.js
@@ -5,11 +5,36 @@ var booksProto = grpc.load('books.proto');
 var client = new booksProto.books.BookService('127.0.0.1:50051',
 	grpc.credentials.createInsecure());
 
-var callbackFunc = function(error, books) {
+function printResponse(error, response) {
 	if (error)
-		console.log('Error: ', error)
+		console.log('Error: ', error);
 	else
-		console.log(books)
+		console.log(response);
 }
 
-client.list({}, callbackFunc);
+function listBooks() {
+	// {} to represent an Empty request message object
+	client.list({}, function(error, books) {
+		printResponse(error, books);
+	});
+}
+
+function insertBook(id, title, author) {
+	var book = { id: parseInt(id), title: title, author: author };
+	client.insert(book, function(error, empty) {
+		printResponse(error, empty);
+	});
+}
+
+function getBook(id) {
+	client.get({ id: parseInt(id) }, function(error, book) {
+		printResponse(error, book);
+	});
+}
+
+function deleteBook(id) {
+	var bookId = { id: parseInt(id) }
+	client.delete(bookId, function(error, epmty) {
+		printResponse(error, empty);
+	});
+}
